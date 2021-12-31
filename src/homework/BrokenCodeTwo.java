53ac0582fd53414e9d479211d055e1a1a54f5d53
joanna <joanna@10.0.0.86>
2021-12-31 13:58:16 -0500

    Broken code HW

---
+++ src/homework/BrokenCodeTwo.java
@@ -0,0 +1,61 @@
+package homework;
+
+public class BrokenCodeTwo {
+	public static void main(String[] args) { 
+		//changed to "public static void main" 
+	//} //this was preventing code from being in main method
+	double tempOne=84.7;//changed to double because int doesnt take decimals
+		 int tempTwo =66;
+		
+		String a1= "result" ;
+		
+		boolean result;
+		//this is a boolean expression
+		if (tempOne <= 0 && tempTwo >= 100) {
+			//there was one extra &, numbers were in quotations as well
+			result = true;
+		}  else  (tempOne >= 100 && tempTwo  0) {
+			//there was an extra if
+			result = true;
+		} else {
+			result= false;
+		}
+		
+		System.out.println("results"); 
+		//added quotations around results 
+		
+		String season = "Monday";
+
+		switch (season== "fall") {
+		case "fall":
+		//got rid of any extra colons
+			System.out.println("Recovering from hot weather");
+			break;
+		case "winter":
+			System.out.println("Toooo cold");			
+			 
+		case "spring":
+		//changed 's' to 'spring'
+		
+	
+			System.out.println("Recovering from cold weather");			
+			break;
+		case "summer":
+			System.out.println("Toooo hot");			
+			 
+		default;
+			System.out.println("Season does not exist!");	
+		}
+		
+		
+		For(Int i; i <10, i++); {
+			
+			if (i = = 5)); (
+				System.out.println("I equals to 5")
+			} else (){
+				System.out.println("i not equals");
+				{	//added quotations around "i not equals"
+				
+					{
+				
+}
